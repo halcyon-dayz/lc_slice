9a3ff82771d90b69392313bd79e584c0b2788017
cmhhelgeson <62450112+cmhhelgeson@users.noreply.github.com>
2022-11-17 15:32:36 -0800

    Added/fixed pointer tests

--- src/features/arrays/tests/arraysSlice.test.ts
+++ src/features/arrays/tests/arraysSlice.test.ts
@@ -101,27 +101,39 @@ describe('arraysSlice redux state tests', () => {
 
     test("if addPointer works with no default location", async () => {
         await store.dispatch(copyArray({data: [1, 2, 3, 4, 5]}));
+        await store.dispatch(copyArray({data: [6, 7, 8, 9, 10]}));
+        let arrays = store.getState().arrays;
+        expect(arrays.length).toEqual(2);
+        //Add three pointers to array one
         let result = await store.dispatch(addPointer({arrayIndex: 0}));
         expect(result.type).toEqual("arrays/addPointer");
         await store.dispatch(addPointer({arrayIndex: 0}));
         await store.dispatch(addPointer({arrayIndex: 0}));
+        //Add two pointers to array two
         await store.dispatch(addPointer({arrayIndex: 1}));
         await store.dispatch(addPointer({arrayIndex: 1}));
-        let arrays = store.getState().arrays
+        arrays = store.getState().arrays
+        //First array should have three pointers
         expect(arrays[0].pointerLocations.length).toEqual(3)
+        //Second array should have 2
         expect(arrays[1].pointerLocations.length).toEqual(2);
+        //Each pointer in array one should have the default position.
         expect(arrays[0].pointerLocations[0]).toEqual(0);
         expect(arrays[0].pointerLocations[1]).toEqual(0);
         expect(arrays[0].pointerLocations[2]).toEqual(0);
-        expect(arrays[1].pointerLocations[2]).toEqual(0);
-        expect(arrays[1].pointerLocations[2]).toEqual(0);
+        //Each pointer in array two should have the default position.
+        expect(arrays[1].pointerLocations[0]).toEqual(0);
+        expect(arrays[1].pointerLocations[1]).toEqual(0);
     })
 
     test("if addPointer works with default location", async () => {
+        //Create two arrays
         await store.dispatch(copyArray({data: [1, 2, 3, 4, 5]}));
+        await store.dispatch(copyArray({data: [5, 6, 7, 8, 9]}));
+        //Add two pointers to array one
         await store.dispatch(addPointer({arrayIndex: 0, location: 3}));
         await store.dispatch(addPointer({arrayIndex: 0, location: 2}));
-
+        //Add a pointer to array two
         await store.dispatch(addPointer({arrayIndex: 1, location: 4}));
         let arrays = store.getState().arrays
         expect(arrays[0].pointerLocations[0]).toEqual(3);
@@ -131,11 +143,21 @@ describe('arraysSlice redux state tests', () => {
 
     test("if addPointer will fail given invalid parameters", async () => {
         await store.dispatch(copyArray({data: [1, 2, 3, 4, 5]}));
+        //Fails due to low index
         await store.dispatch(addPointer({arrayIndex: -1, location: 3}));
+        //Fails due to high index
         await store.dispatch(addPointer({arrayIndex: 40, location: -1}));
+        //Fails and pushes default value
         await store.dispatch(addPointer({arrayIndex: 0, location: -1}));
-        await store.dispatch(addPointer({arrayIndex: -1, location: 5}));
+        //Fails due to low index
+        await store.dispatch(addPointer({arrayIndex: -1, location: -5}));
         let arrays = store.getState().arrays;
-        expect(arrays[0].pointerLocations.length).toEqual(0);
+        expect(arrays[0].pointerLocations.length).toEqual(1);
+        expect(arrays[0].pointerLocations[0]).toEqual(0);
+    })
+
+    test("if movePointer works", async () => {
+        await store.dispatch(copyArray({data: [1, 2, 3, 4, 5]}));
+        await store.dispatch(copyArray({data: [5, 6, 7, 8, 9]}));
     })
 })
