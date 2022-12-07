4daa7b101c52607abf0f2f051a173350216ec29e
cmhhelgeson <62450112+cmhhelgeson@users.noreply.github.com>
2022-12-07 15:51:16 -0800

    Added to enum

--- src/database/databaseUtils.ts
+++ src/database/databaseUtils.ts
@@ -22,3 +22,41 @@ export const changeDimensionsOfGrid = (
   WHERE problem_number = 200;
   `
 }
+
+type CreateGridOnDatabaseProps = {
+  problemNumber: number,
+  data: number[][],
+  label: string,
+}
+
+const genreatePSQL2DARRAY = (data: number[][]) => {
+  let retString = ``;
+  retString += `{`;
+  for (let i = 0; i < data.length; i++) {
+    retString += `{`
+    for (let j = 0; j < data[0].length; j++) {
+      retString += `${data[i][j]}`
+    }
+    if (i !== data.length - 1) {
+      retString += `}\n`
+    } else {
+      retString += `}`
+    }
+  }
+}
+
+export const createGridOnDatabase = ({
+  problemNumber,
+  data,
+  label,
+}: CreateGridOnDatabaseProps) => {
+
+  const dataArray = genreatePSQL2DARRAY(data);
+
+  return `INSERT INTO grids
+  VALUES(${problemNumber},
+  '${dataArray}',
+  ${data[0].length},
+  ${data.length},
+  '${label}'))`;
+}
